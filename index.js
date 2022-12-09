1e9af59c035a9fbd0cda35bccf66a24ccbd72f0d
Seth DImmer <sethdimmer1@gmail.com>
2022-12-09 12:43:09 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=26015f09&s=fast
 
+
 const ratedmoviesElem = document.querySelector('.movies')
 
-async function main() {
-    const movies = await fetch( `https://www.omdbapi.com/?i=tt3896198&apikey=26015f09&s=fast`);
+async function main(movie) {
+    const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=26015f09&s=${movie}`);
     const moviesData = await movies.json();
     console.log(moviesData)
-    moviesData.Search.map((movies) => `<div class="movies">
-    <div class="movie">
-    <figure class="movie__img--wrapper">
-        <img class="movie__img" src="./assets/guardians of the galaxy.jpg" width="77%">
-    </figure>
-    <div class="movie__des">
-        <div class="movie__title"></div>
-        <h3 class="year"></h3>
-        <a href="" class="omdb"></a>
-    </div>
-</div>
-</div>`);
+    ratedmoviesElem.innerHTML = moviesData.Search.map(
+        (movie) =>                      `
+        <div class="movie__container">
+        <div class="movie__wrapper">
+            <div class="movies">
+                <img class="poster" img src="./assets/fast1.jpg">
+                <div class="movie__des">
+                    <h3 class="movie__header"></h3>
+                    <h5 class="year"></h5>
+                    <a href="" class="omdb"></a>
+                </div>
+            </div>
+        </div>
+    </div>`).join("");
 }
 
 main();
