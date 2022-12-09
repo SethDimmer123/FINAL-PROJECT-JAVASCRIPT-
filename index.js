3cc9de567a18a61c75cba9aa5bfb29448df127fb
Seth DImmer <sethdimmer1@gmail.com>
2022-12-09 08:53:17 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=26015f09
+
+const ratedmoviesElem = document.querySelector('.movies')
+
+async function main(movie) {
+    const users = await fetch( `https://www.omdbapi.com/?i=tt3896198&apikey=${movie}`);
+    const moviesData = await movie.json();
+    console.log(usersData);
+
+    ratedmoviesElem.innerHTML = moviesData.map(
+        (movie) => 
+        
+    )
+}
