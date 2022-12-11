d1c11922edbb9d5a7c72316a46ac5ecd06f7a2a5
Seth DImmer <sethdimmer1@gmail.com>
2022-12-11 12:03:50 -0500

    initial commit

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ async function main(movie) {
     const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=26015f09&s=fast`);
     const moviesData = await movies.json();
     console.log(moviesData)
-    ratedmoviesElem.innerHTML = moviesData.Search.slice(0,6).map(
-        (movie) => 
-       `<div class="movie-list">
-        <div class="movie-card">
-            <div class="user-card__container">
-                <figure>
-                    <img src="${movie.Poster}">
-                    <h3 class="">${movie.Title}</h3>
-                    <h4 class="year">${movie.Year}</h4>
-                </figure>
-            </div>
-        </div>
-    </div>`).join("");
+    ratedmoviesElem.innerHTML = moviesData.Search.slice(0,6).map((movie) =>  movieHTML(movie)).join("");
 }
 
 main();
+
+function movieHTML(movie) {
+    return   `<div class="movie-list">
+    <div class="movie-card">
+        <div class="user-card__container">
+            <figure>
+                <img src="${movie.Poster}">
+                <h3 class="">${movie.Title}</h3>
+                <h4 class="year">${movie.Year}</h4>
+            </figure>
+        </div>
+    </div>
+</div>`;
+}
